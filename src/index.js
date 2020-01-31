80c95c6752e23b3040457ef28b4b67172bf4ab65
arcav <armandojcastro84@gmail.com>
2020-01-31 15:53:47 -0400

    index arrowfunction

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import server from './server';
 import '@babel/polyfill/noConflict'
 
 //Start Server
-async function  main()  {
+ const main = async () => {
 	 await server.listen(server.get('port'));
 	 console.log('Server on port',server.get('port')); 	 
  };
- main();
+
+ main()
+ 
+
